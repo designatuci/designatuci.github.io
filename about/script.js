33ace011c59fee421f1d43b8ca9f78d474ebe84f
Kai <KailerGarcia@users.noreply.github.com>
2019-09-30 22:22:47 -0700

    Ready

--- about/script.js
+++ about/script.js
@@ -1,7 +1,24 @@
-$(function() {
+// $(function() {
 
-    $(".person").click(function() {
-        // alert("Click "+$(this))
-    })
+//     $(".person").click(function() {
 
-})
+//         $("body").css({
+//             overflow: 'hidden'
+//         })
+//         $("#modal").removeAttr("hidden")
+//         $("#modal .container").html($(this).html())
+
+//         var firstDivContent = document.getElementById('mydiv1');
+//         var secondDivContent = document.getElementById('mydiv2');
+
+//         // Enable exiting
+//         $("#modalExit").click(function() {
+//             $("#modal").attr("hidden","true")
+//             $("body").css({
+//                 overflow: ''
+//             })
+//         })
+        
+//     })
+
+// })
