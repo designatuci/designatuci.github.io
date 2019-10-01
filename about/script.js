5c8735f702513df41d760e58e8fabe8b1258b9fb
Kai <KailerGarcia@users.noreply.github.com>
2019-10-01 13:02:00 -0700

    Expanding Profiles
    /about/ profiles can now exand on click

--- about/script.js
+++ about/script.js
@@ -1,24 +1,23 @@
-// $(function() {
+$(function() {
 
-//     $(".person").click(function() {
+    $(".person").click(function() {
 
-//         $("body").css({
-//             overflow: 'hidden'
-//         })
-//         $("#modal").removeAttr("hidden")
-//         $("#modal .container").html($(this).html())
+        $(this).attr("hidden","true")
+        $("body").css({
+            overflow: 'hidden'
+        })
+        $("#modal").removeAttr("hidden")
+        $("#modal .container .window").html($(this).html())
 
-//         var firstDivContent = document.getElementById('mydiv1');
-//         var secondDivContent = document.getElementById('mydiv2');
-
-//         // Enable exiting
-//         $("#modalExit").click(function() {
-//             $("#modal").attr("hidden","true")
-//             $("body").css({
-//                 overflow: ''
-//             })
-//         })
+        // Enable exiting
+        $("#modal .exit").click(function() {
+            $("#modal").attr("hidden","true")
+            $(this).removeAttr("hidden")
+            $("body").css({
+                overflow: ''
+            })
+        })
         
-//     })
+    })
 
-// })
+})
