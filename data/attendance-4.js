b6090e953520cb3fb838ffa4295b895d970fd8b3
Kailer Garcia <kailerg@uci.edu>
2019-11-22 11:17:21 -0800

    add attendance 4, 5

---
+++ data/attendance-4.js
@@ -0,0 +1,22 @@
+
+
+attendance[3] = `
+Talon Love
+Christopher Villanueva
+Fernan Lukban
+Vivian Ling
+Jonathan Fung
+Nick Schneider
+Laney Dang
+Kody Ramirez
+Andrew Benson
+Dachelle Alo
+Stella adriana
+Esmeralda Juarez
+Garry Fanata
+Kacy Diep
+Evelyn Liang
+Ryan Chum
+Sabrina Lee
+Saki Tanaka
+`
