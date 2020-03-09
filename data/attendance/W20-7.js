c30d75268041d2fb2ad3c90bcfb8f22a61dc9768
Kailer Garcia <kailerg@uci.edu>
2020-03-09 11:01:56 -0700

    attendnace update

---
+++ data/attendance/W20-7.js
@@ -0,0 +1,10 @@
+winners[6] = [
+    // { name: "Justin Nguyen", winnings: 1 },
+    // { name: "Sophia Huang", winnings: 1 },
+]
+attendance[6] = `
+Christopher Villanueva
+Avineesh Kompella
+Cat Vo
+Justin Nguyen
+`
---
+++ generate/ticketpool/script.js
@@ -0,0 +1,95 @@
+$(function() {
+
+
+    var scores = {}
+
+    // Attendace
+    for (i in attendance) {
+        var people = attendance[i].split("\n")
+        for (j in people) {
+            const name = people[j]
+            if (name in scores) {
+                scores[name] += 1
+            } else {
+                scores[name] = 1
+            }
+        }
+    }
+
+    // Winnings
+    for (i in winners) { var wins = winners[i]
+        for (j in wins) { var p = wins[j]
+            if (p.name in scores) {
+                scores[p.name] += p.winnings
+            } else {
+                scores[p.name] = p.winnings
+            }
+        }
+    }
+
+
+    delete scores[""]
+    display = Object.entries(scores)
+    display = display.sort(function(a,b){
+        if (a[1] == b[1]) {
+            return a[0] > b[0]
+        } else {
+            return a[1] < b[1]
+        }
+    })
+    console.log(display)
+
+    for (i in display) { const entry = display[i]
+        for (var j = 0; j < entry[1]; j += 1) {
+            console.log(entry[0])
+            $("#tickets").append(`
+            <div class="name">${entry[0]}</div>
+            `)
+        }
+
+    }
+
+})
+// $(function() {
+
+
+//     var scores = {}
+
+//     for (i in attendance) {
+//         var people = attendance[i].split("\n")
+//         for (j in people) {
+//             const name = people[j]
+//             if (name in scores) {
+//                 scores[name] += 1
+//             } else {
+//                 scores[name] = 1
+//             }
+//         }
+//     }
+
+
+//     delete scores[""]
+//     display = Object.entries(scores)
+//     display = display.sort((a,b)=>{
+//         if (a[1] == b[1]) {
+//             return a[0] > b[0]
+//         } else {
+//             return a[1] < b[1]
+//         }
+//     })
+//     console.log(display)
+
+//     for (i in display) { const entry = display[i]
+
+//         console.log(entry[0]+" won "+entry[1]+" times")
+//         for (var j = 0; j < entry[1]; j += 1) {
+//             console.log(entry[0])
+//             $("#tickets").append(`
+//             <div class="name">${entry[0]}</div>
+//             `)
+//         }
+        
+
+//     }
+
+// })
