0a5712fdacb3e83984e4d12ad6226db6f9798f3a
kailerg <kailerg@uci.edu>
2020-10-08 10:23:21 -1000

    new designathon webpage

--- designathon/script.js
+++ designathon/script.js
@@ -1,10 +1,18 @@
+$(window).on("load", ()=>{
+
+    $(".link").click((e)=>{
+        $(".link.selected").removeClass("selected")
+        $(e.target).addClass("selected")
+    })
+
+})
+
+
+
 function ready(wait = 0) {
 
     setTimeout(() => {
         document.body.className = "ready"
     }, 200+wait);
 
-    var someText = "cont" + "act@" + "designatuci" + ".com"
-    document.getElementById("m").innerHTML = someText
-    
 }
