a7637818161f99d79a9259060e98185565b70f1b
kailerg <kailerg@uci.edu>
2021-02-22 08:17:38 -1000

    Update schedule.js

--- data/schedule.js
+++ data/schedule.js
@@ -456,7 +456,7 @@ var EVENTS = [
     },
     {
         "id":"OE-21-10",
-        "name":"How to Nail your UX interview",
+        "name":"How to Nail your UX Interview",
         "epoch":1614394800,
         "start":"6:00PM",
         "end":"",
